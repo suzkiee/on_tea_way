6c30470404c3c877804c6ef45b589fe5e07215d2
Suzie K <suziekim.dev@gmail.com>
2021-09-12 16:01:00 -0400

    Green test validations

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,4 +1,9 @@
 class Subscription < ApplicationRecord
   belongs_to :customer
   belongs_to :tea
+
+  validates :title, presence: true
+  validates :price, presence: true, numericality: true
+  validates :status, presence: true
+  validates :frequency, presence: true
 end
