6cde5a2fd9bbe4457ab6cae219e6fd33dea67b07
Suzie K <suziekim.dev@gmail.com>
2021-09-12 15:55:22 -0400

    Green test validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,9 @@
 class Customer < ApplicationRecord
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :address, presence: true
+
+  has_many :subscriptions, dependent: :destroy 
 end
