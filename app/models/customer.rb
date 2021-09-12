119fbc8ed6b67dc0cc31765251c62708b84ec65c
Suzie K <suziekim.dev@gmail.com>
2021-09-12 16:01:15 -0400

    Move relationships above validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,9 +1,9 @@
 class Customer < ApplicationRecord
+  has_many :subscriptions, dependent: :destroy 
+
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :last_name, presence: true
   validates :email, presence: true
   validates :address, presence: true
-
-  has_many :subscriptions, dependent: :destroy 
 end
--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,8 +1,8 @@
 class Tea < ApplicationRecord
+  has_many :subscriptions, dependent: :destroy  
+
   validates :title, presence: true 
   validates :description, presence: true 
   validates :temperature, presence: true, numericality: true 
   validates :brew_time, presence: true, numericality: true 
-
-  has_many :subscriptions, dependent: :destroy  
 end
