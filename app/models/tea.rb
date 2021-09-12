f10b1c2d1b3abf109a1b6d41fa32039fadc6e030
Suzie K <suziekim.dev@gmail.com>
2021-09-12 15:51:56 -0400

    Green test validations

--- app/models/tea.rb
+++ app/models/tea.rb
@@ -1,2 +1,8 @@
 class Tea < ApplicationRecord
+  validates :title, presence: true 
+  validates :description, presence: true 
+  validates :temperature, presence: true, numericality: true 
+  validates :brew_time, presence: true, numericality: true 
+
+  has_many :subscriptions, dependent: :destroy  
 end
