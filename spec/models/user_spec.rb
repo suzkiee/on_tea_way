d2a74fa0bef711c852177de02715434deddcd57e
Suzie K <suziekim.dev@gmail.com>
2021-09-16 13:48:20 -0400

    [Test] Red validations and relationships

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:first_name) }
+    it { should validate_presence_of(:last_name) }
+    it { should validate_uniqueness_of(:email) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:password_digest) }
+  end
+
+  describe 'relationships' do
+    it { should have_many(:subscriptions) }
+  end
+end
