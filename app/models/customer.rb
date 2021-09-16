d7a80f80cb7e2e93e77df7d700f05447e92770f9
Suzie K <suziekim.dev@gmail.com>
2021-09-16 13:49:53 -0400

    [Fix] Rename customers files to users

--- app/models/customer.rb
+++
@@ -1,9 +0,0 @@
-class Customer < ApplicationRecord
-  has_many :subscriptions, dependent: :destroy 
-
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
-  validates :address, presence: true
-end
