8857cdb33420a67c0cc0b4f7e865626595713f08
Suzie K <suziekim.dev@gmail.com>
2021-09-16 20:38:06 -0400

    [Fix] Update to uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
   validates :last_name, presence: true
-  validates :email, presence: true
+  validates :email, uniqueness: true
   validates :address, presence: true
   validates :password_digest, presence: true 
 end
