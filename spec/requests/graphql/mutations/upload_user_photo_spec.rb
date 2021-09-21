915003efa7ecaa1e8ce02ae2dd017bfbf5eec8fa
Suzie K <suziekim.dev@gmail.com>
2021-09-20 22:03:18 -0400

    [Fix] Fix file upload in Happy Path Spec

--- spec/requests/graphql/mutations/upload_user_photo_spec.rb
+++ spec/requests/graphql/mutations/upload_user_photo_spec.rb
@@ -3,22 +3,35 @@ require 'rails_helper'
 RSpec.describe 'Upload User Photo', type: :request do
   describe '.resolve' do
     # Running into issue passing file in the query. Upload works on Altair. 
-    xit 'happy path: uploads file and creates photo' do 
+    it 'happy path: uploads file and creates photo' do 
       user = create(:mock_user) 
-      image = file_fixture('green-blue-pink.jpeg')
+      image = Rack::Test::UploadedFile.new(file_fixture('green-blue-pink.jpeg'), "mime/type")
       query = <<~GQL
                 mutation($image: Upload!) {
                   uploadUserPhoto(input: {
-                        upload: $#{image}
+                        upload: $image
                         userId: #{user.id}}) {
                             userId
                             unsplashId
                             userUploaded
+                            url
                           }
                         } 
                  GQL
-      post '/graphql', params: { query: query }
-      result = JSON.parse(response.body, symbolize_names: true)
+      post '/graphql', params: { query: query, variables: { image: image } }
+      json = JSON.parse(response.body, symbolize_names: true)
+      photo = json[:data][:uploadUserPhoto]
+
+      expect(photo).to have_key(:userId) 
+      expect(photo).to have_key(:unsplashId)
+      expect(photo).to have_key(:userUploaded) 
+      expect(photo).to have_key(:url) 
+      
+      expect(user.photos.first.upload).to be_a(ActiveStorage::Attached::One)
+      expect(photo[:userId].to_i).to eq(user.id)
+      expect(photo[:unsplashId]).to eq(nil)
+      expect(photo[:userUploaded]).to eq(true)
+      expect(photo[:url]).to eq(nil)
     end 
 
     xit 'sad path: uploads file and creates photo' do 
