1a9e45c3f00d987ea68fe6b4a70d0553c284b32e
Suzie K <suziekim.dev@gmail.com>
2021-09-20 21:30:27 -0400

    [Setup] Update seed file for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,41 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Photo.destroy_all
 
-hazel = Customer.create(first_name: "Hazel", last_name: "Leed", email: Faker::Internet.email, address: Faker::Address.full_address )
+user = User.create!(first_name: Faker::Name.first_name, 
+                    last_name: Faker::Name.last_name, 
+                    email: Faker::Internet.email, 
+                    address: Faker::Address.full_address, 
+                    password_digest: Faker::Lorem.sentence )
+
+user_uploaded_1 = user.photos.create!(url: Faker::Internet.url, 
+                                      unsplash_id: nil,
+                                      user_uploaded: true, 
+                                      user_photo_url: 'spec/fixtures/files/green-blue-pink.jpeg',
+                                      user: user)
+user_uploaded_1.upload.attach(io: File.open('spec/fixtures/files/green-blue-pink.jpeg'), filename:'green-blue-pink.jpeg')
+
+user_uploaded_2 = user.photos.create!(url: Faker::Internet.url, 
+                                      unsplash_id: nil,
+                                      user_uploaded: true, 
+                                      user_photo_url: 'spec/fixtures/files/moomin.jpeg',
+                                      user: user)
+user_uploaded_2.upload.attach(io: File.open('spec/fixtures/files/moomin.jpeg'), filename:'moomin.jpeg')
+
+user_uploaded_3 = user.photos.create!(url: Faker::Internet.url, 
+                                      unsplash_id: nil,
+                                      user_uploaded: true, 
+                                      user_photo_url: 'spec/fixtures/files/start-your-morning.jpeg',
+                                      user: user)
+user_uploaded_3.upload.attach(io: File.open('spec/fixtures/files/start-your-morning.jpeg'), filename:'start-your-morning.jpeg')
+
+unsplash_uploaded = user.photos.create!(url: Faker::Internet.url, 
+                                        unsplash_id: "atOlntWcO4k",
+                                        user_uploaded: false, 
+                                        user_photo_url: "https://images.unsplash.com/photo-1600804340584-c7db2eacf0bf?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTEwMzZ8MHwxfHNlYXJjaHwyfHxwdXBweXxlbnwwfHx8fDE2MzIxNDM2MzY&ixlib=rb-1.2.1&q=80&w=1080",
+                                        user: user)
+
+unsplash_uploaded_2 = user.photos.create!(url: Faker::Internet.url, 
+                                          unsplash_id: "fk4tiMlDFF0",
+                                          user_uploaded: false, 
+                                          user_photo_url: "https://images.unsplash.com/photo-1546527868-ccb7ee7dfa6a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyNTEwMzZ8MHwxfHNlYXJjaHwzfHxwdXBweXxlbnwwfHx8fDE2MzIxNDM2MzY&ixlib=rb-1.2.1&q=80&w=1080",
+                                        user: user)
