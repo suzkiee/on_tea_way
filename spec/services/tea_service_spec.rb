ea3b4dd97058beca7e642d2691e52f05bdf4d102
Suzie K <suziekim.dev@gmail.com>
2021-10-03 15:21:20 -0400

    [Test] Red can connect to Tea API

---
+++ spec/services/tea_service_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe TeaService do
+  describe '::find_tes' do
+    it 'connects to Tea API' do 
+      response = TeaService.find_tea("jasmine")
+     
+      expect(response).to be_a(Hash)
+      expect(response).to have_key(:_id)
+      expect(response).to have_key(:name)
+      expect(response).to have_key(:image)
+      expect(response).to have_key(:description)
+      expect(response).to have_key(:keywords)
+      expect(response).to have_key(:origin)
+      expect(response).to have_key(:brew_time)
+      expect(response).to have_key(:temperature)
+      expect(response).to have_key(:comments)
+      expect(response).to have_key(:__v)
+      
+      expect(response[:comments][0]).to have_key(:date)
+      expect(response[:comments][0]).to have_key(:_id)
+      expect(response[:comments][0]).to have_key(:text)
+    end 
+
+  end 
+end 
